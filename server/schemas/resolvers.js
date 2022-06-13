710b3b1da5e1e30d1916669f066b690d4b368cce
Yutthana00 <arf101293@gmail.com>
2022-06-13 04:59:46 +0100

    remove book mutation link added with auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -56,7 +56,18 @@ const resolvers = {
       }
       throw new AuthenticationError("You need to be logged in!");
     },
-    
+    removeBook: async (_parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
 };
 
 module.exports = resolvers;
